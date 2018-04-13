3abd1c77904fe5bccfed61a12f42acb629f1b314
Tasnim Khandakar <tasnimk@berkeley.edu>
2018-04-12 19:28:32 -0700

    added necessary files and pkgs

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-# Released to students
+#!/usr/local/bin/python
 
 import sys
 sys.path.append('..')
@@ -11,12 +11,29 @@ import numpy as np
 from student_utils_sp18 import *
 # import planarity
 
+# Function to algorithmically write matrix
 
+# Open file and write the adjancency matrix in it and then close it
 def test(input_file) :
     file_object = open("input_file", "w")
-    file.write("hello world")
-    file.write("this is our new text file")
-    
 
+    ####### REPLACE TWO LINES WITH ACTUAL MATRIX CODE ########
+    file_object.write("hello world\n")
+    file_object.write("this is our new text file")
+    ##########################################################
+
+
+
+    # CLOSE FILE AFTER OPENING
+    file_object.close()
+
+
+
+# Main function
+def main():
+    test("input.txt")
+
+
+# Keep for script
 if __name__ == '__main__':
-    test()
+    main()
