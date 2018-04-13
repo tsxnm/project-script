544e05fa55dcfbdf2895ed5fcaa67547f01084ae
Tasnim Khandakar <tasnimk@berkeley.edu>
2018-04-12 19:00:56 -0700

    added script.py

---
+++ script.py
@@ -0,0 +1,22 @@
+# Released to students
+
+import sys
+sys.path.append('..')
+sys.path.append('../..')
+import argparse
+import utils
+import networkx as nx
+import numpy as np
+# from utils_sp18 import *
+from student_utils_sp18 import *
+# import planarity
+
+
+def test(input_file) :
+    file_object = open("input_file", "w")
+    file.write("hello world")
+    file.write("this is our new text file")
+    
+
+if __name__ == '__main__':
+    test()
